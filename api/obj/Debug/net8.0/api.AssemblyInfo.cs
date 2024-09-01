db18c83d02bff2de7ea106685211b10f4e26ed00
Dominic Perrine <86677707+perrinedominic@users.noreply.github.com>
2024-09-01 10:31:18 -0500

    Comment and Stock Models

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44fe6874b91d6d945e85ca6c6ee2a2d1dfc481e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5da55390a60c6040d5a5d627922e069f4da864f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
