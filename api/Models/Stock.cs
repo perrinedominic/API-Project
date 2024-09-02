51f917e70c8b0b9fa3d381a06a3ae3c762951eab
Dominic Perrine <86677707+perrinedominic@users.noreply.github.com>
2024-09-02 03:12:09 -0500

    Add Stock Controller

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -9,14 +9,27 @@ namespace api.Models
     public class Stock
     {
         public int Id { get; set; }
-        public string Services { get; set; } = String.Empty;
-        public string CompanyName { get; set; } = String.Empty;
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Purchase { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal LastDiv { get; set; }
+
+        [Column(TypeName = "char(3)")]
+        public string Exchange { get; set; }
+
+        [Column(TypeName = "char(5)")]
+        public string Symbol { get; set; } = String.Empty;
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; } = String.Empty;
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string Sector { get; set; } = String.Empty;
+        [Column(TypeName = "nvarchar(150)")]
         public string Industry { get; set; } = String.Empty;
-        public long MarketCap { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
+        public decimal CurrentPrice { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
+        public decimal MarketCap { get; set; }
+
         public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
