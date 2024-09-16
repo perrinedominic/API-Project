1c954d3fc70e68f524aaadd57a910f5b54cbd157
Dominic Perrine <86677707+perrinedominic@users.noreply.github.com>
2024-09-15 19:28:55 -0500

    Added the comment system.

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Comment CommentModel)
+        {
+            return new CommentDto
+            {
+                Id = CommentModel.Id,
+                Title = CommentModel.Title,
+                Content = CommentModel.Content,
+                CreatedOn = CommentModel.CreatedOn,
+                StockId = CommentModel.StockId,
+            };
+        }
+    }
+}
